e25de2f1857e36606ace54b08f956e03c6cfdc8d
Sercan ISLI <sercanislii@gmail.com>
2023-08-13 18:05:50 +0300

    Add project files.

---
+++ Core.Persistance/Repositories/IAsyncRepository.cs
@@ -0,0 +1,65 @@
+﻿using Core.Persistance.Dynamic;
+using Core.Persistance.Paging;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+
+namespace Core.Persistance.Repositories
+{
+    public interface IAsyncRepository<TEntity,TEntityId> : IQuery<TEntity>
+        where TEntity:Entity<TEntityId>
+    {
+        Task<TEntity?> GetAsync(
+            Expression<Func<TEntity,bool>> predicate,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include=null,
+            bool withDeleted = false,
+            bool enableTracking = true,
+            CancellationToken cancellationToken = default 
+            );
+
+        Task<Paginate<TEntity>> GetListAsync(
+           Expression<Func<TEntity, bool>>? predicate = null,
+           Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null, 
+           Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+           bool withDeleted = false,
+           int index = 0,
+           int size = 10,
+           bool enableTracking = true,
+           CancellationToken cancellationToken = default 
+            );
+
+        Task<Paginate<TEntity>> GetListByDynamicAsync(
+           DynamicQuery dynamic,
+           Expression<Func<TEntity, bool>>? predicate = null,
+           Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+           bool withDeleted = false,
+           int index = 0,
+           int size = 10,
+           bool enableTracking = true,
+           CancellationToken cancellationToken = default
+       );
+
+        Task<bool> AnyAsync(
+            Expression<Func<TEntity, bool>>? predicate = null,
+            bool enableTracking = true,
+            bool withDeleted = false,
+            CancellationToken cancellationToken = default
+            );
+
+        Task<TEntity> AddAsync(TEntity entity);
+
+        //Task<IList<TEntity>> AddRangeAsync(IList<TEntity> entity);
+        Task<ICollection<TEntity>> AddRangeAsync(ICollection<TEntity> entities);
+
+        Task<TEntity> UpdateAsync(TEntity entity);
+
+        Task<ICollection<TEntity>> UpdateRangeAsync(ICollection<TEntity> entities);
+
+        //Task<IList<TEntity>> UpdateRangeAsync(IList<TEntity> entity);
+
+        Task<TEntity> DeleteAsync(TEntity entity, bool permanent = false);
+
+        //Task<IList<TEntity>> DeleteRangeAsync(IList<TEntity> entity);
+
+        Task<ICollection<TEntity>> DeleteRangeAsync(ICollection<TEntity> entities, bool permanent = false);
+    }
+}
---
+++ Core.Persistance/Repositories/IRepository.cs
@@ -0,0 +1,48 @@
+﻿using Core.Persistance.Dynamic;
+using Core.Persistance.Paging;
+using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistance.Repositories
+{
+    public interface IRepository<TEntity, TEntityId> : IQuery<TEntity>
+     where TEntity: Entity<TEntityId>
+    {
+        TEntity? Get(
+            Expression<Func<TEntity, bool>> predicate,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+            bool enableTracking = true
+        );
+
+        Paginate<TEntity> GetList(
+            Expression<Func<TEntity, bool>>? predicate = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+            int index = 0,
+            int size = 10,
+            bool enableTracking = true
+        );
+
+        Paginate<TEntity> GetListByDynamic(
+            DynamicQuery dynamic,
+            Expression<Func<TEntity, bool>>? predicate = null,
+            Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+            int index = 0,
+            int size = 10,
+            bool enableTracking = true
+        );
+
+        bool Any(Expression<Func<TEntity, bool>>? predicate = null, bool enableTracking = true);
+        TEntity Add(TEntity entity);
+        IList<TEntity> AddRange(IList<TEntity> entities);
+        TEntity Update(TEntity entity);
+        IList<TEntity> UpdateRange(IList<TEntity> entities);
+        TEntity Delete(TEntity entity);
+        IList<TEntity> DeleteRange(IList<TEntity> entity);
+    }
+}
