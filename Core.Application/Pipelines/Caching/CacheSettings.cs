ed1ad962f0ff3fd29ded93bc3f9dbc309b8b8e34
Sercan ISLI <sercanislii@gmail.com>
2023-08-20 19:33:36 +0300

    Cache infrastructure created.

---
+++ Core.Application/Pipelines/Caching/CacheSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.Application.Pipelines.Caching
+{
+    public class CacheSettings
+    {
+        public int SlidingExpiratiton { get; set; }
+    }
+}
---
+++ Core.Application/Pipelines/Caching/CachingBehavior.cs
@@ -0,0 +1,54 @@
+﻿using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Core.Application.Pipelines.Caching
+{
+    public class CachingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>, ICachableRequest
+    {
+        private readonly CacheSettings _cacheSettings;
+        private readonly IDistributedCache _cache;
+
+        public CachingBehavior(IDistributedCache cache, CacheSettings cacheSettings)
+        {
+            _cache = cache;
+            _cacheSettings = cacheSettings;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (request.BypassCache)
+            {
+                return await next();
+            }
+            TResponse response;
+            byte[]? cachedResponse = await _cache.GetAsync(request.CacheKey, cancellationToken); 
+            if(cachedResponse != null)
+            {
+                response = JsonSerializer.Deserialize<TResponse>(Encoding.Default.GetString(cachedResponse));
+            }
+            else
+            {
+                response = await GetResponseAndAddToCache(request, next, cancellationToken);
+            }
+            return response;
+        }
+
+        private async Task<TResponse?> GetResponseAndAddToCache(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            TResponse response = await next();
+            TimeSpan? slidingExpiration = request.SlidingExpiration ?? TimeSpan.FromDays(_cacheSettings.SlidingExpiratiton);
+            DistributedCacheEntryOptions cacheOptions = new() { SlidingExpiration= slidingExpiration };
+            byte[] serializedData = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(response));
+            await _cache.SetAsync(request.CacheKey, serializedData, cacheOptions, cancellationToken);
+
+            return response;
+        }
+    }
+}
