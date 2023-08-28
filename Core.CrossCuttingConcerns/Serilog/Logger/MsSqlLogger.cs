aa9c60042071e8c182feba6d9dda936ea72e177b
Sercan ISLI <sercanislii@gmail.com>
2023-08-28 18:25:27 +0300

    MSSQL Implementation was done.

--- Core.CrossCuttingConcerns/Serilog/Logger/MsSqlLogger.cs
+++ Core.CrossCuttingConcerns/Serilog/Logger/MsSqlLogger.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Core.CrossCuttingConcerns.Serilog.ConfigurationModels;
+using Core.CrossCuttingConcerns.Serilog.Messages;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +13,23 @@ namespace Core.CrossCuttingConcerns.Serilog.Logger
 {
     public class MsSqlLogger:LoggerServiceBase
     {
+        public MsSqlLogger(IConfiguration configuration)
+        {
+            MsSqlLogConfiguration logConfiguration = configuration.GetSection("SeriLogConfiguration:MsSqlConfiguration")
+               .Get<MsSqlLogConfiguration>() ?? throw new Exception(SerilogMessages.NullOptionsMessage);
 
+            MSSqlServerSinkOptions sinkOptions = new()
+            {
+                TableName = logConfiguration.TableName,
+                AutoCreateSqlDatabase = logConfiguration.AutoCreateSqlTable
+            };
+
+            ColumnOptions columnOptions = new();
+
+            global::Serilog.Core.Logger serilogConfiguration = new LoggerConfiguration().WriteTo
+                .MSSqlServer(logConfiguration.ConnectionString, sinkOptions, columnOptions:columnOptions).CreateLogger();
+
+            Logger = serilogConfiguration;
+        }
     }
 }
